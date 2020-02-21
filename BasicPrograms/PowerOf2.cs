04139b02997fa7da572ca55a55a4ea47c0fe01d4
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:22:36 +0530

    [Rajesha T K] added PowerOf1.cs implementation of POWER OF 2.cs Program

---
+++ BasicPrograms/PowerOf2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class PowerOf2
+    {
+        public PowerOf2()
+        {
+            PrintPower();
+        }
+        static void PrintPower()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the number: ");
+            int num = u.ReadInt();
+            for(int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
