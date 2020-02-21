a003183eb4b25a3bc0de53ec96a6962f779417e3
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:38:25 +0530

    [Rajesha T K] added Program.cs It contained Main method of all Classes

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsharpPrograms.LogicalPrograms;
+
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the Class Number: ");
+                int num = u.ReadInt();
+            switch (num)
+            {
+                case 1: new Gambler(); break;
+                case 2: new CoupanNumber(); break;
+                case 3: new StopWatch(); break;
+                case 4: new TicTacGame(); break;
+                case 5: new TicTacGameByUsingOneDArray();break;
+                default: Console.WriteLine("No such Class Name: ");
+                    break;
+            }
+        }
+    }
+}
