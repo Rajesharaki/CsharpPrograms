d5e79abb73b030fcf70c520e1f53ba5205e61d6e
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:52:12 +0530

    [Rajesha T K] added GuessNumber.cs implementation of GuessNumber Program

---
+++ AlgorithmPrograms/GuessNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.AlgorithmPrograms
+{
+    class GuessNumber
+    {
+        public GuessNumber()
+        {
+            Game();
+        }
+        static void Game()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the number: ");
+            int num = u.ReadInt();
+            int range=(int)Math.Pow(2, num);
+            Console.WriteLine("Choose a Number Between 0 to "+ (range-1));
+           int number= Guess(0,range);
+            Console.WriteLine("Your number is : " + number);
+        }
+        static int Guess(int f,int l)
+        {
+            if ((l - f) == 1)
+            {
+                return f;
+            }
+            int mid = f+(l-f) / 2;
+            Console.WriteLine("Is it your number is less than (true/false) " + mid+"?");
+            if (bool.Parse(Console.ReadLine()))
+            {
+                return Guess (f, mid);
+            }
+            else
+            {
+               return  Guess(mid, l);  
+            }
+        }
+    }
+}
