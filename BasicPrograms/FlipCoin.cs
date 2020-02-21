9252bcaec3578fe4eb577f5f8cbd1400e14eaddd
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:19:18 +0530

    [Rajesha T K] added FlipCoin.cs implementation of FlipCoin Program

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class FlipCoin
+    {
+        public FlipCoin()
+        {
+            FlipGame();
+        }
+        static void FlipGame()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter how many times you want to flip coin: ");
+            int t = u.ReadInt();
+            Random rc = new Random();
+            int head = 0, tail = 0;
+            if (t > 0)
+            {
+                for(int i = 1; i <= t; t--)
+                {
+                    double rf = rc.NextDouble();
+                    Console.WriteLine("ramdom number id:" + rf);
+                    if (rf < 0.5)
+                    {
+                        Console.WriteLine("Tail");
+                        tail++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Head");
+                        head++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Coin not Flipped.............");
+            }
+            Console.WriteLine("Percentage of Head/tail is:" + (head / tail));
+        }
+    }
+}
