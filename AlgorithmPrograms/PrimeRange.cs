67e3c276305febed8e07e636cc7b5cfdba24ba8a
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:58:29 +0530

    [Rajesha T K] added PrimeProgram.cs implementation of PrimeRange Program

---
+++ AlgorithmPrograms/PrimeRange.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.AlgorithmPrograms
+{
+    class PrimeRange
+    {
+        public PrimeRange()
+        {
+            PrintPrime();
+        }
+        static void PrintPrime()
+        {
+            Utility u = new Utility();
+            Console.WriteLine("Enter the range: ");
+            Console.Write("Enter First range value: ");
+            int Low = u.ReadInt();
+            Console.Write("Enter the Second range value: ");
+            int High = u.ReadInt();
+            if (Low > High)
+            {
+              
+                int temp = Low;
+                Low = High;
+                High = temp;
+            }
+            for (int i = Low; i <= High; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static bool IsPrime(int num)
+        {
+           
+            bool flag = true;
+            for(int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+    }
+}
