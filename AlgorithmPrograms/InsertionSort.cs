56fe44e331dfd5100b84e3e85bcdc12772f0bb17
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:53:23 +0530

    [Rajesha T K] added InsertionSort.cs implementation of InsertionSort Program

---
+++ AlgorithmPrograms/InsertionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.AlgorithmPrograms
+{
+    class InsertionSort
+    {
+        public InsertionSort()
+        {
+            Sorting();
+        }
+        static void Sorting()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the Size Of an array: ");
+            int Size =u.ReadInt();
+            String[] a = new String[Size];
+            //array filling
+            for(int i = 0; i < Size; i++)
+            {
+                Console.Write("Enter the Only String value: ");
+                a[i]= Console.ReadLine();
+            }
+            //insertion sort
+            for(int i = 1; i < Size; i++)
+            {
+                string temp = a[i];
+                int j = i - 1;
+                while (j >= 0 && a[j].CompareTo(temp) > 0)
+                {
+                    a[j + 1] = a[j];
+                    j--;
+                }
+                a[j + 1] = temp;
+            }
+            //Sorted value display
+            String st = "{";
+            for(int i = 0; i < Size; i++)
+            {
+                st += a[i] + ",";
+            }
+            st += "}";
+            Console.WriteLine(st);
+        }
+    }
+}
