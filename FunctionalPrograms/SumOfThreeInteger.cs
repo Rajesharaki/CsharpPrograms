5633ff82c726d409309c77a268b6d6dba9bf3503
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:32:36 +0530

    [Rajesha T K] added SumOfThree.cs implementation of Sum Of three integer Program

---
+++ FunctionalPrograms/SumOfThreeInteger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CsharpPrograms.FunctionalPrograms
+{
+    class SumOfThreeInteger
+    {
+      public SumOfThreeInteger()
+        {
+            CallMethod();
+        }
+        static void CallMethod()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the Size Of an array: ");
+            int size = u.ReadInt();
+            int[] a = new int[size];
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.Write("Enter the values: ");
+                a[i] = u.ReadInt();
+            }
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for(int j = 0; j < a.Length; j++)
+                {
+                    for(int k = 0; k < a.Length; k++)
+                    {
+                        if (a[i] + a[j] + a[k] == 0)
+                        {
+                            Console.WriteLine( "distinct triplates are: " +a[i] + " " + a[j] +" " + a[k]);
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("number of triplets: " + count);
+        }
+    }
+}
