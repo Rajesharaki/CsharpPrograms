e7f5dc01b6978116217304b070ce048106ed0a5d
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 19:04:42 +0530

    [Rajesha T K] added Sqrt.cs implementation of Sqrt Program

---
+++ JUnitPrograms/Sqrt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.JUnitTestingPrograms
+{
+    class Sqrt
+    {
+        public Sqrt()
+        {
+            FindRoot();
+        }
+        static void FindRoot()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the number: ");
+            double c = u.ReadDouble();
+            Double t = c;
+            double epsilon = 1 * Math.Pow(10, -15);
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (t + c / t)/2;
+            }
+            Console.WriteLine("Square root of " + c + " is: " + t);
+
+        }
+    }
+}
