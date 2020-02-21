771b39eeda2c71242f8cfaec4c6531d784e4b09f
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:57:00 +0530

    [Rajesha T K] added PermutationString.cs implementation of PermutationString Program

---
+++ AlgorithmPrograms/PermutationString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace CsharpPrograms.AlgorithmPrograms
+{
+    class PermutationString
+    {
+        public PermutationString()
+        {
+            Permutation();
+        }
+        static void Permutation()
+        {
+            Console.Write("Enter the String: ");
+            String str=Console.ReadLine();
+            Permute(str, 0, str.Length - 1);
+        }
+        static void Permute(String str,int f,int l)
+        {
+            if (f == l)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for(int i = f; i <= l; i++)
+                {
+                    str=Swap(str, f, i);
+                    Permute(str, f + 1, l);
+                    str = Swap(str, f, i);
+                }
+            }
+            static string Swap(String str,int i,int j)
+            {
+                Char[]a=str.ToCharArray();
+                char temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                string s = new string(a);
+                return s;
+            }
+        }
+    }
+}
