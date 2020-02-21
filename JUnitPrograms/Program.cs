d4d42bb91c0835cd1a5ee977104f2e3b42891450
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 19:01:44 +0530

    [Rajesha T K] added Program.cs It contained Main method of all Classes

---
+++ JUnitPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CsharpPrograms.JUnitTestingPrograms;
+
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the Class Number: ");
+                int num = u.ReadInt();
+            switch (num)
+            {
+                case 1: new TemperaturConversion(); break;
+                case 2: new MonthlyPayment(); break;
+                case 3: new Sqrt(); break;
+                case 4: new ToBinary(); break;
+                case 5: new SwappNibbles(); break;
+                case 6: new VendingMachine(); break;
+                default: Console.WriteLine("No such Class Name: ");
+                    break;
+            }
+        }
+    }
+}
