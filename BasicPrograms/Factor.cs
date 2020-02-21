66b3a93079e28af9e6f880bba6d727a0c8122111
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:18:24 +0530

    [Rajesha T K] added Factor.cs implementation of Factor Program

---
+++ BasicPrograms/Factor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class Factor
+    {
+        public Factor()
+        {
+            IsFactor();
+        }
+        static void IsFactor()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the Number: ");
+            int num = u.ReadInt();
+            for(int i=2;i<=num;i++)
+            {
+                while(num%2==0)
+                {
+                    Console.Write(i+" ");
+                    num /= 2;
+                }
+                if(num>0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
