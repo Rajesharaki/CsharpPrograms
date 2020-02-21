375bb139fbf878457d3b522c1438ab081c630f36
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:27:28 +0530

    [Rajesha T K] added EuclideanDistance.cs implementation of EuclideanDistance Program

---
+++ BasicPrograms/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms
+{
+   public class Utility
+    {
+        public int ReadInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        public double ReadDouble()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+        public float ReadFloat()
+        {
+            return float.Parse(Console.ReadLine());
+        }
+        public long ReadLong()
+        {
+            return long.Parse(Console.ReadLine());
+        }
+        public bool ReadBoolean()
+        {
+            return bool.Parse(Console.ReadLine());
+        }
+        public char ReadChar()
+        {
+            return char.Parse(Console.ReadLine());
+        }
+    }
+}
+    
+
