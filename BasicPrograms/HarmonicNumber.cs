dd4a8a08ce11a2ad4de1933e5b664f0b838d5951
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-21 18:20:08 +0530

    [Rajesha T K] added HarmonicNumber.cs.cs implementation of FlipCoin Program

---
+++ BasicPrograms/HarmonicNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPrograms.BasicPrograms
+{
+    class HarmonicNumber
+    {
+        public HarmonicNumber()
+        {
+            IsHarmonic();
+        }
+        static void IsHarmonic()
+        {
+            Utility u = new Utility();
+            Console.Write("Enter the  n th number: ");
+            int num = u.ReadInt();
+            if (num > 0)
+            {
+                double sum = 0.0;
+                for(double i = 1; i <= num; i++)
+                {
+                    sum += 1 / i;
+                }
+                Console.WriteLine(num + " th Harmonic number is = " + sum);
+            }
+        }
+    }
+}
