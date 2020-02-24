8fa6a05e973f00c53f56b5bae4a20abb9e2f03a3
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-24 18:49:46 +0530

    [Rajesha T K] added FileReader.cs Implementation of FileRead Program

---
+++ DataStructurePrograms/FileReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class FileRead
+    {
+        public FileRead()
+        {
+            Read();
+        }
+        static void Read()
+        {
+            Console.Write("Enter the searching word: ");
+            String Word = Console.ReadLine();
+            UnOrderedList < String > List= new UnOrderedList<String>();
+            String FilePath = @"C:\Users\rajes\OneDrive\Desktop\Demo.txt";
+            String []  a=File.ReadAllText(FilePath).Split(' ');
+            for(int i = 0; i < a.Length; i++)
+            {
+                List.Add(a[i]);
+            }
+            int Lenght = List.Size();
+            if (List.Search(Word))
+            {
+                List.Remove(Word);
+            }
+            else
+            {
+                List.Append(Word);
+            }
+            String st = "";
+            for(int i = 0; i < List.Size(); i++)
+            {
+                st += List.Peek(i)+" ";
+            }
+            File.WriteAllText(FilePath,st);
+        }
+         
+          
+    }
+}
