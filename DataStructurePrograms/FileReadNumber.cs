a28b52517e831e376bd211a0fe9d519a54627451
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-25 18:07:42 +0530

    [Rajesha T K] added FileReadNumber.cs Implementation of FileRead Program

---
+++ DataStructurePrograms/FileReadNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DataStructurePrograms
+{
+    class FileReadNumber
+    {
+        public FileReadNumber()
+        {
+            Read();
+        }
+        static void Read()
+        {
+            string FilePath = @"C:\Users\rajes\OneDrive\Desktop\File.txt";
+            String[]a=File.ReadAllText(FilePath).Split(' ');
+            OrderedList list = new OrderedList();
+            //added value from array to string
+            for(int i = 0; i < a.Length; i++)
+            {
+                list.Add(int.Parse(a[i]));
+            }
+            Console.Write("Enter the searching value: ");
+            int num = int.Parse(Console.ReadLine());
+            if (list.Search(num))
+            {
+                list.Remove(num);
+            }
+            else
+            {
+                list.Add(num);
+            }
+            String st = "";
+            for (int i = 0; i < list.Size(); i++)
+            {
+                st += list.Peek(i)+" ";
+            }
+            File.WriteAllText(FilePath, st);
+
+        }
+    }
+}
