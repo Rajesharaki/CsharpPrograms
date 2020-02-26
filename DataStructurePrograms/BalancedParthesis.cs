8ba0e2255b6636f5e75aeb9be70e2264ebc105a0
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-26 11:38:42 +0530

    [Rajesha T K] added BalancedParthesis.cs Implementation of Balanced Parthesis Program

---
+++ DataStructurePrograms/BalancedParthesis.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class BalancedParthesis
+    {
+        public BalancedParthesis()
+        {
+           if(Check())
+            {
+                Console.WriteLine("Entered Expression is Balanced......");      
+            }
+            else
+            {
+                Console.WriteLine("Entered Expression is not Balanced......");
+            }
+        }
+
+        static bool Check()
+        {
+            Console.Write("Enter the arthametic Expression: ");
+            String exp = Console.ReadLine();
+            Stack<char> list = new Stack<char>(exp.Length);
+            for(int i = 0; i < exp.Length; i++)
+            {
+                char ch = exp[i];
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+
+                    list.Push(ch);
+                }
+                else if (ch == ')' || ch == ']' || ch == '}')
+                {
+                    if (list.IsEmpty())
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        char c = list.Pop();
+                        if(ch==')'&&c!='('|| ch == ']' && c != '[' ||ch == '}' && c != '{')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return list.IsEmpty();
+
+        }
+    }
+}
