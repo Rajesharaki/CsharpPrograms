054c3260c6c5f41c9faf9a43e03925316e224e89
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-27 15:02:59 +0530

    [Rajesha T K] Refactor. Implemenatation of Palindrome checker program

---
+++ DataStructures/Dequeue/PalindromeChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class PalindromeChecker
+    {
+        public PalindromeChecker()
+        {
+            Check();
+        }
+        public static void Check()
+        {
+            Console.Write("Enter the  String: ");
+            String Word = Console.ReadLine();
+            DeQueue<char> list = new DeQueue<char>(Word.Length);
+
+            //add char into the que
+            for(int i = 0; i < Word.Length; i++)
+            {
+                list.AddRear(Word[i]);
+            }
+            //Check Palindrome
+            while (!(list.IsEmpty()))
+            {
+                if (list.Size() == 1)
+                {
+                    break;
+                }
+                if (!(list.RemoveFront().Equals(list.RemoveRear())))
+                {
+                    Console.WriteLine("'"+Word + "' is not a Palindrome word.....");
+                    Environment.Exit(0);
+                }
+            }
+            Console.WriteLine("'"+Word + "' is a Palindrome Word......");
+        }
+    }
+}
