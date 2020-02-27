9ab4c31fb04ebe22c20a82578f26f8bb504e8421
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-27 18:40:52 +0530

    [Rajesha T K]add. Implementation of FileReader program code

---
+++ DataStructures/UnorderedList/FileReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class FileRead
+    {
+        public FileRead()
+        {
+            Read();
+        }
+        static void Read()
+        {
+            Console.Write("Enter the searching word: ");
+            String Word = Console.ReadLine();
+            //create UnOrderedList object
+            
+            UnOrderedList < String > List= new UnOrderedList<String>();
+            String FilePath = @"C:\Users\rajes\OneDrive\Desktop\Demo.txt";
+            
+            //Read all lines from the text and it returns string array
+            String []  a=File.ReadAllText(FilePath).Split(' ');
+            
+            //Element added from Array to List
+            for(int i = 0; i < a.Length; i++)
+            {
+                List.Add(a[i]);
+            }
+            int Lenght = List.Size();
+            
+            //If Element found delete that elment from the List
+            if (List.Search(Word))
+            {
+                List.Remove(Word);
+            }
+            
+            //Else add the element to the list
+            else
+            {
+                List.Append(Word);
+            }
+            String st = "";
+            
+            //Convert List to string
+            for(int i = 0; i < List.Size(); i++)
+            {
+                st += List.Peek(i)+" ";
+            }
+            
+            //added all text into the text file
+            File.WriteAllText(FilePath,st);
+        }
+         
+          
+    }
+}
