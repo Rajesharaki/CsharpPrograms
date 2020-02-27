610a2212185aa1a33d80945ac0a7ee9f5ebc7d51
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-27 18:13:11 +0530

    [Rajesha T K] Refactor . Modified RemoveFront method code

--- DataStructures/Dequeue/DeQueue.cs
+++ DataStructures/Dequeue/DeQueue.cs
@@ -19,7 +19,7 @@ namespace DataStructurePrograms
             if (Rear == Array.Length - 1)  //If Rear is More than Queue Size   
             {
                 Console.WriteLine("Queue is OverFlow....."); 
-                return;
+                return; 
             }
             else
             {
