1deffe771f8e541b7993e6c033becdcc0019977e
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-02-28 14:41:20 +0530

    [Rajesha T K] add . Implementation Of BinarySearch Tree Add method Code

---
+++ DataStructures/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePrograms
+{
+    class BinarySerchTree
+    {
+        //Creating Node class
+        class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public Node(int Data)
+            {
+                this.Data = Data;
+            }
+        }
+        Node Root;
+        //Adds a Items into the binary tree
+        public void Add(int Data)
+        {
+            Node n=new Node(Data);
+            if (Root == null)      //If root is empty then root pointing to the newly created object
+            {
+                Root = n;
+            }
+            Node Current = Root;
+            Node Parent = Current;
+            while(Current != null)   //Finding the position to Add a items into the Balanced tree
+            {
+                Parent=Current;
+                if (n.Data < Current.Data)
+                {
+                    Current = Current.Left;
+                }
+                else
+                {
+                    Current = Current.Right;
+                }
+            }
+            if (n.Data < Parent.Data) //If N data is Less than Parent Data then Add item into the Left side of Parent
+            {
+                Parent.Left = n;
+            }
+            else                       //Else Add item into Right side of Parent
+            {
+                Parent.Right = n;
+            }
+        }
+    }
+}
