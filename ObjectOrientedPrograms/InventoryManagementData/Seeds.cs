4fda84b5d748191927b2dd2f73feaab4bb8f823b
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-06 15:58:53 +0530

    [Rajesha T K] Refactor. Modified Seeds class code

--- ObjectOrientedPrograms/InventoryManagementData/Seeds.cs
+++ ObjectOrientedPrograms/InventoryManagementData/Seeds.cs
@@ -4,24 +4,25 @@ using System.Text;
 
 namespace ObjectOrientedPrograms.InventoryManagementData
 {
-    public class Inventory
+    public class InventoryFactory
     {
-        public int Sum { get; set; }
+
         public List<Seeds> Rice;
         public List<Seeds> Pulses;
         public List<Seeds> Wheats;
+      
     }
-   public class Seeds
+    public class Seeds
     {
-
-            public string brand;
-            public int PricePerKg;
-            public int Weight;
-            public int TotalPrice;
-            public override string ToString()
-            {
-                return string.Format("name:\t{0}\nPrice per KG:\t{1}\nWeight:\t{2}\nTotalPrice:\t{3}", this.brand, this.PricePerKg, this.Weight, this.TotalPrice);
-            }
+        public String Name;
+        public long Weight;
+        public long Price;
+        public long TotalPrice;
+        public string ToString()
+        {
+            string st = $"Name:{Name}\nWeight:{Weight}\nPrice:{Price}\nTotalPrice{TotalPrice}";
+            return st;
         }
     }
+}
 
