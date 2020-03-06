ebca9af48872aadc8e99dba9edd270968f21d528
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-06 18:17:28 +0530

    [Rajesha T K] add. implementation of StockPortFoili class program code

---
+++ ObjectOrientedPrograms/StockAccountManagement/StockPortFoili.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPrograms.StockAccountManagement
+{
+    class Stock
+    {
+        public String StockName;
+        public long NumberOfShare;
+        public long SharePrice;
+        public long TotalStockPrice;
+        public override string ToString()
+        {
+            string st = $"StockName: {StockName}\nNumberOfShare: {NumberOfShare}\nSharePrice: {SharePrice}\nTotalStockPrice: {TotalStockPrice}";
+            return st;
+        }
+    }
+    class StockPortFoili
+    {
+        public List<Stock> Stocks;
+        public long GrandTotalStockPrice;
+    }
+}
