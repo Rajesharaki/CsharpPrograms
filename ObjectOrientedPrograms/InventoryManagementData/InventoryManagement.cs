c3317d0182fb22c5ace9be92591faea0d7392579
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-06 15:59:57 +0530

    [Rajesha T K] Refactor. Added Display Method code

--- ObjectOrientedPrograms/InventoryManagementData/InventoryManagement.cs
+++ ObjectOrientedPrograms/InventoryManagementData/InventoryManagement.cs
@@ -8,155 +8,150 @@ namespace ObjectOrientedPrograms.InventoryManagementData
 {
     class InventoryManagement
     {
+        InventoryFactory iv;
+        string FilePath = @"C:\Users\yempc72\Desktop\Inventory.json";
         public InventoryManagement()
         {
-            Console.WriteLine("Welcome to Inventory MAnagement");
-            while (true)
-            {
-                Console.WriteLine("Enter A to Add\tEnter D to delete an Item");
-                char entered = char.Parse(Console.ReadLine());
-                switch (entered)
-                {
-                    case 'A': Add(); break;
-                    case 'D': Delete(); break;
-                    default: Console.WriteLine("Enter a valid character"); break;
-                }
-                Console.WriteLine("enter yes/No to conitnue");
-                if (Console.ReadLine() != "yes")
-                {
-                    break;
-                }
-            }
+            InventoryManagements();
         }
-
-        public void Add()
+        public void InventoryManagements()
         {
-            ////fetching the json file
-            string jfile = File.ReadAllText(@"C:\Users\yempc72\Desktop\DataFile.json");
-            ////creating the inventory object and assgning the deserialized value to it
-            Inventory iv;
-            iv = (Inventory)JsonConvert.DeserializeObject<Inventory>(jfile);
+            Console.WriteLine("1--> AddItems 2--> Display 3--> quit");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1: AddItems(); break;
+                case 2: Display(); break;
+                case 3: Environment.Exit(0); break;
+                default:
+                    Console.WriteLine("Entered wrong number ..Enter agsin");
+                    InventoryManagements();
+                    break;
+            }
+
+        }
+        public void AddItems()
+        {
+            Console.WriteLine("1-->Add Rice Items\n2-->Add Pulses Items\n3-->Add Wheats items");
+            int Selection = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Brand Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Weight: ");
+            long Weight = long.Parse(Console.ReadLine());
+            Console.Write("Enter the Price per Kg: ");
+            long Price = long.Parse(Console.ReadLine());
+            Seeds seed = new Seeds()
+            {
+                Name = name,
+                Weight = Weight,
+                Price = Price,
+                TotalPrice = Price * Weight
+            };
+            string FilePath = @"C:\Users\yempc72\Desktop\Inventory.json";
+            string Read = File.ReadAllText(FilePath);
             if (iv == null)
             {
-                iv = new Inventory();
+                iv = new InventoryFactory();
             }
-            int sum = 0;
-            if (iv != null)
+            if (Read.Length != 0)
             {
-                sum = iv.Sum;
+                iv = JsonConvert.DeserializeObject<InventoryFactory>(Read);
             }
-            ////creating a Seeds object to fill it in switch based on requirement
-            Seeds item = new Seeds();
-            ////asking the user to choose the given option
-            Console.WriteLine("Enter 1--> for Rice\tEnter 2--> for Pulses\tEnter 3--> for Wheats\t");
-            int entered = int.Parse(Console.ReadLine());
-            ////filling the details
-            Console.Write("Enter the name of the Product: ");
-            item.brand = Console.ReadLine();
-            Console.Write("Enter the Price per Kg: ");
-            item.PricePerKg = int.Parse(Console.ReadLine());
-            Console.Write("Enter the Weight: ");
-            item.Weight = int.Parse(Console.ReadLine());
-            item.TotalPrice = item.PricePerKg * item.Weight;
-            if (iv != null)
-            {
-                sum += item.TotalPrice;
-            }
-            else
-            {
-                sum = item.TotalPrice;
-            }
-            ////running a  based on user
-            switch (entered)
+            switch (Selection)
             {
                 case 1:
                     if (iv.Rice == null)
                     {
                         iv.Rice = new List<Seeds>();
                     }
-                    iv.Rice.Add(item);
+                    iv.Rice.Add(seed);
                     break;
                 case 2:
                     if (iv.Pulses == null)
                     {
                         iv.Pulses = new List<Seeds>();
                     }
-                    iv.Pulses.Add(item);
+                    iv.Pulses.Add(seed);
                     break;
                 case 3:
                     if (iv.Wheats == null)
                     {
                         iv.Wheats = new List<Seeds>();
                     }
-                    iv.Wheats.Add(item);
+                    iv.Wheats.Add(seed);
                     break;
                 default:
-                    Console.WriteLine("Invalid Entry try Again...");
+                    Console.WriteLine("Entered Wrong number...Enter again....");
+                    AddItems();
                     break;
             }
-            iv.Sum = sum;
-            ////now serializing and writing to file directly 
-            using (System.IO.StreamWriter writer = File.CreateText(@"C:\Users\yempc72\Desktop\DataFile.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(writer, iv);
-                Console.WriteLine("new Product Added to the Inventory");
-            }
+            string data = JsonConvert.SerializeObject(iv);
+            File.WriteAllText(FilePath, data);
+            InventoryManagements();
         }
-        public void Delete()
+        public void Display()
         {
-            Console.WriteLine("Are you sure you Want to delete...items from Inventory: ");
-            string jfile = File.ReadAllText(@"C:\Users\yempc72\Desktop\DataFile.json");
-            Inventory iv = JsonConvert.DeserializeObject<Inventory>(jfile);
-            Console.WriteLine("Enter 1--> for Rice\tEnter 2--> for Pulses\tEnter 3--> for Wheats\t");
-            int entered = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the name of the brand");
-            string brand = Console.ReadLine();
-            int sum = iv.Sum;
-            switch (entered)
+            String Read = File.ReadAllText(FilePath);
+            iv = JsonConvert.DeserializeObject<InventoryFactory>(Read);
+            Console.WriteLine("1--> RiceDetails \n2--> Pulses Details\n3--> Wheats Details\n4-->All Details\n5-->AddItems\nQuit(Any number)");
+            int select = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (select)
             {
                 case 1:
-                    foreach (Seeds s in iv.Rice)
+                    Console.WriteLine("...................Rice Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var rice in iv.Rice)
                     {
-                        if (s.brand.Equals(brand))
-                        {
-                            sum -= s.TotalPrice;
-                            iv.Rice.Remove(s);
-                            break;
-                        }
+                        Console.WriteLine(rice.ToString() + "\n");
                     }
                     break;
                 case 2:
-                    foreach (Seeds s in iv.Pulses)
+                    Console.WriteLine("...................Pulses Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var pulses in iv.Pulses)
                     {
-                        if (s.brand.Equals(brand))
-                        {
-                            sum -= s.TotalPrice;
-                            iv.Rice.Remove(s);
-                            break;
-                        }
+                        Console.WriteLine(pulses.ToString() + "\n");
                     }
                     break;
                 case 3:
-                    foreach (Seeds s in iv.Wheats)
+                    Console.WriteLine("...................Wheats Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var wheats in iv.Wheats)
                     {
-                        if (s.brand.Equals(brand))
-                        {
-                            sum -= s.TotalPrice;
-                            iv.Rice.Remove(s);
-                            break;
-                        }
+                        Console.WriteLine(wheats.ToString() + "\n");
                     }
                     break;
+                case 4:
+                    Console.WriteLine("...................Rice Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var rice in iv.Rice)
+                    {
+                        Console.WriteLine(rice.ToString() + "\n");
+                    }
+                    Console.WriteLine("...................Pulses Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var pulses in iv.Pulses)
+                    {
+                        Console.WriteLine(pulses.ToString() + "\n");
+                    }
+                    Console.WriteLine("...................Wheats Details...............");
+                    Console.WriteLine("_______________________________________________");
+                    foreach (var wheats in iv.Wheats)
+                    {
+                        Console.WriteLine(wheats.ToString() + "\n");
+                    }
+                    break;
+                case 5:
+                    AddItems();
+                    break;
                 default:
-                    Console.WriteLine("there is no such brand available in Inventory");
+                    Environment.Exit(0);
                     break;
             }
-            using (StreamWriter stream = File.CreateText(@"C:\Users\yempc72\Desktop\DataFile.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(stream, iv);
-            }
+            Display();
         }
     }
+
 }
+
