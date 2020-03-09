9c072fd82f5adc946683244ae793a4fae346b3b1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 18:59:30 +0530

    [Rajesha T K] add. Implemenatation of StaticBlockInitialization Program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/StaticBlockIntialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    public class StaticBlockIntialization
+    {
+        private static StaticBlockIntialization Instance;
+        private StaticBlockIntialization()
+        {
+
+        }
+        //Static block to intialixing the exception handling
+}
