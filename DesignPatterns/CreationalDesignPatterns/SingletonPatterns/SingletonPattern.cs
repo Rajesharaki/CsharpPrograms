db5735ae332c374211b888251bfd8d02aaefc709
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 12:01:05 +0530

    [Rajesha T K] add. implemenatation of singletonPattern program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/SingletonPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    /// <summary>
+    /// If we want only one object of any class we go for Singleton Pattern
+    /// </summary>
+    class SingletonPattern
+    {
+        //Make object reference static why because static method can handle only static members
+        private static SingletonPattern Instance;
+
+        //Make constructor private why because we can't create object out side the class
+        private SingletonPattern()
+        {
+
+        }
+
+        /*Make GetInstance method public and static if method is public we can access anywhere and method is static 
+        without creating an object we can accecss that method */
+        public static SingletonPattern GetInstance
+        {
+            get //Getter method
+            {
+                if (Instance == null) //if Instace is null create instance of singleton class if not then return previous instance
+                {
+                    Instance = new SingletonPattern();
+                }
+                return Instance;
+            }
+        }
+    }
+}
