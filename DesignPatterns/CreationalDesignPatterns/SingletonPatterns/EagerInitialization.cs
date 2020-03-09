3af248c4acf51a573403250a8a0f3d2c213e59b5
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 18:57:54 +0530

    [Rajesha T K] add. Implemenatation of EagerInitialization Program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/EagerInitialization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    public class EagerInitialization
+    {
+        private static EagerInitialization instance = new EagerInitialization();
+        private EagerInitialization()
+        {
+
+        }
+        public static EagerInitialization GetInstance()
+        {
+            return instance;
+        }
+    }
+}
