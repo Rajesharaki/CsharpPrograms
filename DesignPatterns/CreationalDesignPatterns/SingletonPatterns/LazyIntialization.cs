685cfbe7862264d3a5ae627bbb2f1c3ce65490f3
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 18:58:51 +0530

    [Rajesha T K] add. Implemenatation of LazyInitialization Program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/LazyIntialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    class LazyIntialization
+    {
+        private static LazyIntialization Instance;
+        private LazyIntialization() { }
+        public LazyIntialization GetInsatnce()
+        {
+            Instance = null;
+            if (Instance == null)
+            {
+                Instance = new LazyIntialization();
+            }
+            return Instance;
+        }
+    }
+}
