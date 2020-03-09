63fca4438fd89875e445a202cb4e3d35f658ed48
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 15:38:08 +0530

    [Rajesha T K]add. Implementation of Program class code

---
+++ DesignPatterns/CreationalDesignPatterns/FactoryPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.CreationalDesignPatterns.FactoryPattern;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Product Name: ");
+            Computer Product = ComputerFactory.GetInstance(Console.ReadLine());
+            Product.Produce();
+        }
+    }
+}
