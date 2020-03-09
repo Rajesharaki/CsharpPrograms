6d77f596c905121375fdcc7965504496b491e28e
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-09 11:59:42 +0530

    [Rajesha T k] add. implemenation of Program class code

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.CreationalDesignPatterns.SingletonPatterns;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var object1=SingletonPattern.GetInstance;
+            var object2 = SingletonPattern.GetInstance;
+            Console.WriteLine(object1.GetHashCode());
+            Console.WriteLine(object1.GetHashCode());
+        }
+    }
+}
