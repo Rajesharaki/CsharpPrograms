9b90750f8e95d68afd16b15ea4535b10d55aeca5
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 19:20:03 +0530

    [Rajesha T K]add . Implementation of Reflection program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/Reflection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    sealed class Reflection : Demo
+    {
+        private Reflection()
+        {
+
+        }
+        public static Reflection GetInstance
+        {
+            get
+            {
+                return ReflectionHelperClass.Instance;
+            }
+        }
+        private sealed class ReflectionHelperClass
+        {
+            public static Reflection Instance = new Reflection();
+        }
+    }
+}
