9676d234b074c63dfcc80aa264b2727e372dc806
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 16:49:20 +0530

    [Rajesha T K] add . Implementation of DoublyCheckedLocking program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/DoublyCheckedLocking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    sealed class DoublyCheckedLocking
+    {
+        private static Object ObjectInstance = new object();
+        private static DoublyCheckedLocking Instance;
+        private static int count = 0;
+        private DoublyCheckedLocking() { Console.WriteLine("Count: " + (++count)); }
+       
+        public static DoublyCheckedLocking GetDoublyCheckedLockingInsatnce
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (ObjectInstance)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DoublyCheckedLocking();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+}
