a4b19bf2b8958d8a7630a725dcbfb2fb8f378b0a
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 18:25:08 +0530

    [Rajesha T K] add. Implementation of StaticIntialazation Program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/StaticIntialazationSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    sealed class StaticIntialazationSingleton
+    {
+        private static StaticIntialazationSingleton Instance = new StaticIntialazationSingleton();
+        private StaticIntialazationSingleton() { }
+        public static StaticIntialazationSingleton GetStaticIntialazationSingleton
+        {
+          
+            get
+            {
+                int a,b;
+                Console.Write("Enter the value: ");
+                int.TryParse(Console.ReadLine(), out a);
+                Console.Write("Enter the value: ");
+                int.TryParse(Console.ReadLine(), out b);
+                try
+                {
+                    return Instance;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Exception occured in creating singleton instance");
+                }
+            }
+        }
+    }
+}
