da499e8648543bb3f35178ce2b160243117b6652
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 13:23:32 +0530

    [Rajesha T K] add . Implemenatation of ThreadSafe Method code program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/ThreadSafeSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    public sealed class ThreadSafeSingleton
+    {
+        private static  Object Locker = new Object();
+        private static ThreadSafeSingleton Instance;
+        private ThreadSafeSingleton() { }
+        public static ThreadSafeSingleton GetInstance()
+        {
+            if (Instance == null)
+            {
+                lock (Locker)
+                {
+                    if (Instance == null)
+                    {
+                        Instance = new ThreadSafeSingleton();
+                    }
+                }
+            }
+            return Instance;
+        }
+    }
+}
