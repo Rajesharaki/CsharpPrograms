b95e45465a990f677e90adfcbfa77dc602666f5e
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 16:57:09 +0530

    [Rajesha T k] add . modified GetInstance method code

--- DesignPatterns/CreationalDesignPatterns/SingletonPatterns/LazyIntialization.cs
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/LazyIntialization.cs
@@ -5,18 +5,38 @@ using System.Threading;
 
 namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
 {
+    /// <summary>
+    /// Make class sealed because if we make class seled nested class also not possible to inhert this class
+    /// </summary>
     public sealed class LazyIntialization
     {
+        /*Create one static variable that can hold Lazy Intialization object*/
         private static LazyIntialization Instance;
-        private LazyIntialization() { }
-        public static LazyIntialization GetInstance()
+
+        static int count = 0; //just for check How many objects are create
+
+        /*Make Constructor private because if we make constructor private out side the class we can't create object and that class doesn't have any child class 
+        (means inheritance not possible)*/
+        private LazyIntialization() { Console.WriteLine("Count: " + (++count)); } //Checking how many objects are create
+      
+        /*Make method public and static  beacuse if public it's golbal point access method and if it's static we can accecss that method without create an object
+          this pattern work fine for single thread in multithreading it's create more than one object */
+        public static LazyIntialization GetInstance
         {
-            if (Instance == null)
+            get
             {
-                Instance = new LazyIntialization();
+                if (Instance == null) 
+                {
+                    Instance = new LazyIntialization();
+                }
+                return Instance;
             }
-            return Instance;
         }
 
+        //Just for checking 
+        public void  PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
     }
 }
