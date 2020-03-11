2b87be3f77816670c4871626a0d4c7c768ecc493
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 16:58:28 +0530

    [Rajesha T K] Refactor. Modified GetInstance method code

--- DesignPatterns/CreationalDesignPatterns/SingletonPatterns/ThreadSafeSingleton.cs
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/ThreadSafeSingleton.cs
@@ -8,10 +8,16 @@ namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
     {
         private static readonly  Object Locker = new Object();
         private static ThreadSafeSingleton Instance;
-        private ThreadSafeSingleton() { }
-        public static ThreadSafeSingleton GetInstance()
+        static int count = 0;
+
+        /*Make Constructor private because if we make constructor private out side the class we can't create object and that class doesn't have any child class 
+        (means inheritance not possible)*/
+        private ThreadSafeSingleton() { Console.WriteLine("Count: " + (++count)); }
+
+        /*if we make method synchronize then it's create only one object in multithreading or single threading but drawback is if we make method synchornize then performance will reduce*/
+        public static ThreadSafeSingleton GetInstance
         {
-            if (Instance == null)
+            get
             {
                 lock (Locker)
                 {
@@ -20,8 +26,12 @@ namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
                         Instance = new ThreadSafeSingleton();
                     }
                 }
+                return Instance;
             }
-            return Instance;
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
