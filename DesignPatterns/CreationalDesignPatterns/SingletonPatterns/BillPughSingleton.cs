cde040867b2f1ab507db72307bcdf07ef9d0af4e
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-11 17:25:27 +0530

    [Rajesha T K]add . Implementation of BillPughSingleton Program code

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPatterns/BillPughSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPatterns
+{
+    public sealed class BillPughSingleton
+    {
+        static int count = 0;
+        private BillPughSingleton() { Console.WriteLine("Count: "+(++count)); }
+        public static BillPughSingleton GetPughSingletonInstance
+        {
+            get
+            {
+                return BillPughInnerClass.Instance;
+            }
+        }
+        private sealed class BillPughInnerClass
+        {
+            public static BillPughSingleton Instance = new BillPughSingleton(); 
+        }
+    }
+}
