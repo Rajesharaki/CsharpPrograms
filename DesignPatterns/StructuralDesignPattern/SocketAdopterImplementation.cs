700686f91c447d8a5c5fd27e06314b44da4010f1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-12 19:29:24 +0530

    [Rajesha T K]add. Implementation of SocketAdopterImplementation code

---
+++ DesignPatterns/StructuralDesignPattern/SocketAdopterImplementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern
+{
+
+    /// <summary>
+    /// Implementation of Scoket adopter interface
+    /// </summary>
+    public class SocketAdopterImplementation : ISocketAdopter
+    {
+        Socket obj = Socket.GetSocketInstance; //Creating Socket class object
+        public int GetVolt120()
+        {
+            return obj.GetVolt(); //Producing 120volts
+        }
+
+        public int GetVolt12() //Adopter convert 120volts to 12 volts 
+        {
+            return Converter(obj,10); //120/10=12;
+        }
+
+        public int GetVolt3() //Adopter convert 120volts to 3 volts
+        {
+            return Converter(obj, 40);
+        }
+
+        //Converting Socket volts to required volts
+        public int Converter(Socket Socketobj,int RequiredVolt)
+        {
+            int volt=(Socketobj.GetVolt()) / RequiredVolt;
+            return volt;
+        }
+    }
+}
