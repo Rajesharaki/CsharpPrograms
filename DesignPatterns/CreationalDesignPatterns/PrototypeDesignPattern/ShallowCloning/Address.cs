1273586dee7e22a60724439ba9fc88083f07c7db
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-12 16:02:16 +0530

    [Rajesha T K]add. Implementation of Address program code

---
+++ DesignPatterns/CreationalDesignPatterns/PrototypeDesignPattern/ShallowCloning/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypeDesignPattern.ShallowCloning
+{
+    public class Address : Cloning
+    {
+        public override Cloning GetClone()
+        {
+            return (Address)this.MemberwiseClone();
+        }
+
+        public override void GetDetails()
+        {
+            Console.WriteLine($"Address: {this.address}");
+        }
+    }
+}
