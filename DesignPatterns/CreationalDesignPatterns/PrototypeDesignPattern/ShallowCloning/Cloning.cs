1467a8389a77029b27ee15789e42a20ee1d3677f
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-12 16:03:59 +0530

    [Rajesha T K]add. Implementation of Shallow clonning program code

---
+++ DesignPatterns/CreationalDesignPatterns/PrototypeDesignPattern/ShallowCloning/Cloning.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypeDesignPattern.ShallowCloning
+{
+    /// <summary>
+    /// Create abstract class 
+    /// </summary>
+    public abstract class Cloning
+    {
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public string address { get; set; }
+        public abstract Cloning GetClone();
+        public abstract void GetDetails();
+    }
+}
