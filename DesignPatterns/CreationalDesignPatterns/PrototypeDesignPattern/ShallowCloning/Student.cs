92add3171601ea53d708057770bccfc367865bd1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-12 16:05:08 +0530

    [Rajesha T K] add . Implementation of Student program code

---
+++ DesignPatterns/CreationalDesignPatterns/PrototypeDesignPattern/ShallowCloning/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypeDesignPattern.ShallowCloning
+{
+    //shallow clonning
+    public class Student :Cloning
+    {
+        public string USN { get; set; }
+       
+        public override Cloning GetClone()
+        {
+            return (Student)this.MemberwiseClone(); // By using superclass reference copy the student object
+        }
+
+        public override void GetDetails()
+        {
+            Console.WriteLine($"Student Name : {this.Name}\nUSN:{this.USN}\nDept: {this.Dept}");
+        }
+    }
+}
