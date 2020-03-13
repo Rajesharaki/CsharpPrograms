71edeac8a75cfd199213e0b75190eeba8d7ef964
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 18:42:30 +0530

    [Rajesha T K]add. Implementation of Testing Program code

---
+++ DesignPatterns/BehavioralDesignPattern/ObeserverDesignPattern/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern
+{
+    public class Testing
+    {
+        static void Main(String [] args)
+        {
+            ISubject flipkart = new Flipkart();
+            IObserver instance = new PersonOne("Rajesha T K");
+            flipkart.Register(instance);
+            IObserver instance2 = new PersonOne("Sanju");
+            flipkart.Register(instance2);
+            IObserver instance1 = new PersonTwo("Madan T K");
+            flipkart.Register(instance1);
+            flipkart.ProvideAvailablity("available");
+            flipkart.ProvideAvailablity("Notavailable");
+        }
+    }
+}
