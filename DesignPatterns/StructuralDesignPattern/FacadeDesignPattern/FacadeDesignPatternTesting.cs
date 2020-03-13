b9d717a42572957be598c383df9df1928eb596b9
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 11:14:34 +0530

    [Rajesha T K]add. Implementation of FacadeDesignPatternTesting program code

---
+++ DesignPatterns/StructuralDesignPattern/FacadeDesignPattern/FacadeDesignPatternTesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.FacadeDesignPattern
+{
+    public class FacadeDesignPatternTesting
+    {
+        static void Main(String [] args)
+        {
+            Console.WriteLine("Display address from json file");
+            Console.WriteLine();
+            DisplayAddress.FromJsonFile();
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Display address from Text file");
+            Console.WriteLine();
+            DisplayAddress.FromTextFile();
+        }
+    }
+}
