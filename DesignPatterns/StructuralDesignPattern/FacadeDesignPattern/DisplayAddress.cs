7c5cfcea528ef64489f2f53c51c928781a52a456
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 11:09:48 +0530

    [Rajesha T K]add. Implemenatation of DisplayAddress program code

---
+++ DesignPatterns/StructuralDesignPattern/FacadeDesignPattern/DisplayAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.FacadeDesignPattern
+{
+    /// <summary>
+    /// Facade Design pattern minimize the complexity and client can easly interact with subsystem
+    /// </summary>
+    public static class DisplayAddress
+    {
+        static IReadFiles Instance;
+
+        //Calling JsonFileReader
+        public static void FromJsonFile()
+        {
+            Instance = new JsonFileReader();
+            Instance.ReadFile();
+        }
+        //Calling TextFileReader
+        public static void FromTextFile()
+        {
+            Instance = new TextFileReader();
+            Instance.ReadFile();
+        }
+    }
+}
