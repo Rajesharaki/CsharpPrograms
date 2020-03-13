107fb347173c190d9faab93ac7627011cc300112
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 11:06:18 +0530

    [Rajesha T K]Refactor. Modified Folder

---
+++ DesignPatterns/StructuralDesignPattern/AdapterDesignPattern/ISocketAdopter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern
+{
+
+    //Create ISocketAdopter interface Its adopter B/W Socket and Required voltage convereter
+    public interface ISocketAdopter
+    {
+        public int GetVolt120(); //Adopter produce 120volts
+        public int GetVolt12();//Adopter produce 12volts
+        public int GetVolt3();//Adopter produce 3volts
+    }
+}
---
+++ DesignPatterns/StructuralDesignPattern/AdapterDesignPattern/Testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern
+{
+    class Testing
+    {
+        /*static void Main(String [] args)
+        {
+            ISocketAdopter Insatnce = new SocketAdopterImplementation();
+            Console.WriteLine(Insatnce.GetVolt120());
+            Console.WriteLine(Insatnce.GetVolt12());
+            Console.WriteLine(Insatnce.GetVolt3());
+        }*/
+    }
+}
