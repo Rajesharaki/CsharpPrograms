fbc845e1cd180ecaab7b8e53956a65ab6ec18687
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 18:14:26 +0530

    [Rajesha T K]add. Implementation of FlipKart program code

---
+++ DesignPatterns/BehavioralDesignPattern/ObeserverDesignPattern/Flipkart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern
+{
+    public class Flipkart : ISubject
+    {
+        public string ProductName = "Redmi";
+        public int Price = 10000;
+        public string Available = "Available";
+        List<IObserver> Details;
+        public void Notify()
+        {
+            foreach (var notify in Details)
+            {
+                Flipkart instance = new Flipkart();
+                notify.Update(instance);
+            }
+        }
+
+        public void ProvideAvailablity(string Available)
+        {
+            if (Available.ToLower() == "available")
+            {
+                Notify();
+            }
+            else
+            {
+                Console.WriteLine("Out of Stock");
+            }
+        }
+        public void Register(IObserver subscribe)
+        {
+            if (Details == null)
+            {
+                Details = new List<IObserver>();
+            }
+            Details.Add(subscribe);
+        }
+
+        public void UnRegister(IObserver subscribe)
+        {
+            if (Details.Contains(subscribe))
+            {
+                Details.Remove(subscribe);
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+    }
+}
