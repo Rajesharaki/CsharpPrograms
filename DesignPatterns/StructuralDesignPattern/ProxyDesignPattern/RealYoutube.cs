83d78b586237433f1e8071e0ec19648aba85e810
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 14:40:27 +0530

    [Rajesha T K]add. Implementation of RealYoutube program code

---
+++ DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/RealYoutube.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
+{
+    public class RealYoutube : IYoutube
+    {
+        public void DownloadVideo()
+        {
+
+            Console.WriteLine("video Download Completed....");
+        }
+    }
+}
