06e35df03c7bd817b8c292ebd5e7ef52d9da07cf
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 11:15:34 +0530

    [Rajesha T K]add .  Implemenatation of IReadFile Program code

---
+++ DesignPatterns/StructuralDesignPattern/FacadeDesignPattern/IReadFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.FacadeDesignPattern
+{
+    /// <summary>
+    /// Interface 
+    /// </summary>
+    public interface IReadFiles
+    {
+        public void ReadFile();
+    }
+}
