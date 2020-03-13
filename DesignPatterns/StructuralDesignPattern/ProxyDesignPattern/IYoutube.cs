c27a913ae5853c522d1af342a43e934833328c02
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 14:37:34 +0530

    [Rajesha T K]add. Implementation of IYoutube Program code

---
+++ DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/IYoutube.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
+{
+    public interface IYoutube
+    {
+        public void DownloadVideo();
+    }
+}
