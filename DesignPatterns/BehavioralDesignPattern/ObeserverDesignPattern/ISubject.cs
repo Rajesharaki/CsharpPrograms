71d2bb0d87b5b0d6141052b4c16f3ee450cff552
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 18:16:30 +0530

    [Rajesha T k]add. Implementation of ISubject program code

---
+++ DesignPatterns/BehavioralDesignPattern/ObeserverDesignPattern/ISubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern
+{
+    public interface ISubject
+    {
+       
+        public void Register(IObserver subscribe);
+        public void UnRegister(IObserver subscribe);
+        public void ProvideAvailablity(String Available);
+    }
+}
