e368b9a53008a2721933e46bdb8ccc46b6ca6e74
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 14:38:46 +0530

    [Rajesha T K]add. Implementation of ProxyYoutube Program code

---
+++ DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/ProxyYoutube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
+{
+    public class ProxyYoutube : IYoutube
+    {
+        RealYoutube Instance;
+        public void DownloadVideo()
+        {
+            if (Instance == null)
+            {
+                Instance = new RealYoutube();
+            }
+            Instance.DownloadVideo();
+        }
+    }
+}
