3bad562a65c9a7eea1961860a99978185eacf65d
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 14:36:25 +0530

    [Rajesha T K]add. Implementation Client Program code

---
+++ DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
+{
+    class Client
+    {
+        static void Main(String [] args)
+        {
+            IYoutube instance = new ProxyYoutube();
+            instance.DownloadVideo();
+        }
+    }
+}
