2e4dcb792dc78af3978eb3ec63aff199a7abd3c1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-13 11:19:48 +0530

    [Rajesha T K]add. Implementation of TextFileReader program code

---
+++ DesignPatterns/StructuralDesignPattern/FacadeDesignPattern/TextFileReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DesignPatterns.StructuralDesignPattern.FacadeDesignPattern
+{
+    public class TextFileReader : IReadFiles
+    {
+
+        //Reading address from text file
+        public void ReadFile()
+        {
+            string ReadFile=File.ReadAllText(@"C:\Users\yempc72\Desktop\VisualStudioPrograms\Project\DesignPatterns\StructuralDesignPattern\FacadeDesignPattern\TextFileAddressBook.txt");
+            Console.WriteLine(ReadFile);
+        }
+    }
+}
