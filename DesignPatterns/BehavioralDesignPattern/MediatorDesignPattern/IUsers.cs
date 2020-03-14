b081367a42ca6927c479295b745608ab37a62cd7
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 12:10:19 +0530

    [Rajesha T K]add. Implementation of Iuser Interface code

---
+++ DesignPatterns/BehavioralDesignPattern/MediatorDesignPattern/IUsers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.MediatorDesignPattern
+{
+    public abstract class IUsers
+    {
+       public IMediator mediator;
+        public string Name;
+        public IUsers(IMediator mediator, string name)
+        {
+            this.mediator = mediator;
+            this.Name = name;
+        }
+        public abstract void Receive(string message);
+        public abstract void Send(string message );
+    }
+}
