d2909e9b772c569c730cfd395c303025be8fe26a
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 12:12:28 +0530

    [Rajesha T K]add. Implementation of UserImplementation Code

---
+++ DesignPatterns/BehavioralDesignPattern/MediatorDesignPattern/UsersImpl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.MediatorDesignPattern
+{
+    public class UsersImpl : IUsers
+    {
+        public UsersImpl(IMediator mediator,string name) :base(mediator, name)
+        {
+         
+        }
+        public override void Receive(string message)
+        {
+            Console.WriteLine(this.Name + ": Received Message:  " + message);
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine(this.Name + ":Sending Message: " + message+"\n\n");
+            mediator.SendMessage(message,this);
+        }
+    }
+}
