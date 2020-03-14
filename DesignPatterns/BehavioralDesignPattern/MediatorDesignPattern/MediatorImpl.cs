027170273829eb5d3d2f981ffa4e10b9dedc0038
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 12:11:22 +0530

    [Rajesha T K]add. Implementation of Mediator program code

---
+++ DesignPatterns/BehavioralDesignPattern/MediatorDesignPattern/MediatorImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.MediatorDesignPattern
+{
+    public class MediatorImpl : IMediator
+    {
+        List<IUsers> users = new List<IUsers>();
+        public void Register(IUsers user)
+        {
+            users.Add(user);
+        }
+
+        public void SendMessage(string message,IUsers user)
+        {
+            foreach(var send in users)
+            {
+                if (send != user)
+                {
+                    send.Receive(message);
+                }
+            }
+        }
+    }
+}
