146d3ba4b4e6c0ac1969147c5060a385d6c966ea
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:15:56 +0530

    [Rajesha T K]add. Implementation of Vegitable program code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/Vegitables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    public class Vegitables:IItemElement
+    {
+        public string VegitablesName;
+        public int price;
+        public double Weight;
+        public Vegitables(string VegitablesName, int price, double weight)
+        {
+            this.VegitablesName = VegitablesName;
+            this.price = price;
+            this.Weight = weight;
+        }
+        public double accept(IShoppingCartVisitor Visitor)
+        {
+            return Visitor.Visit(this);
+        }
+    }
+}
