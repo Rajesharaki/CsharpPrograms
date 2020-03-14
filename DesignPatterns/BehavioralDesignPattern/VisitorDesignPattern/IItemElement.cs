3ff8005516007a51c752b6725dd7ef15db701288
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:12:19 +0530

    [Rajesha T K] add . Implementation of IItemElement interface code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/IItemElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    public interface IItemElement
+    {
+        public double accept(IShoppingCartVisitor Visitor);
+    }
+}
