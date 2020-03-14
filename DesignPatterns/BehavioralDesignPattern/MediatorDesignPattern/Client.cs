1c14fded8195879975f1d071db942d3bbb7a6d3a
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 12:08:01 +0530

    [Rajesha T K]add. Implementation of Client program code

---
+++ DesignPatterns/BehavioralDesignPattern/MediatorDesignPattern/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.MediatorDesignPattern
+{
+    class Client
+    {
+        static void Main(String [] args)
+        {
+            IMediator MediatorInstance = new MediatorImpl();
+            IUsers Rajesha = new UsersImpl(MediatorInstance, "Rajesha");
+            IUsers Madan= new UsersImpl(MediatorInstance, "Madan");
+            IUsers Sanju = new UsersImpl(MediatorInstance, "Sanju");
+            IUsers Sundar = new UsersImpl(MediatorInstance, "Sundar");
+            IUsers Rohith = new UsersImpl(MediatorInstance, "Rohith");
+          
+            MediatorInstance.Register(Madan);
+            MediatorInstance.Register(Sanju);
+            MediatorInstance.Register(Sundar);
+            MediatorInstance.Register(Rohith);
+            Rajesha.Send("Hi How are you guys");
+        }
+    }
+}
