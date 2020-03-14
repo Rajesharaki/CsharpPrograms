f98a3a065b157ff1cd00bec1580ef0bd4f17de78
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:13:39 +0530

    [Rajesha T K]add. Implementation of IShopping cart visitor interface code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/IShoppingCartVisitor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    public interface IShoppingCartVisitor
+    {
+        public double Visit(Fruits fruit);
+        public double Visit(Vegitables veg);
+    }
+}
