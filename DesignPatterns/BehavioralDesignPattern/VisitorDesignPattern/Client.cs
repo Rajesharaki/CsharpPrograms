103e79c0da76c426b3b87e6b07f5c991f0953160
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:09:45 +0530

    [Rajesha T K]add . Implementation of Client Program code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    class Client
+    {
+        public static void Main(String [] args)
+        {
+            IItemElement[] items = new IItemElement[] {new Fruits("Apple",100,2),new Vegitables("Tamoto",60,1) };
+            double total = Calculation(items);
+            Console.WriteLine("TotalAmount: " + total);
+        }
+        private static double Calculation(IItemElement[] items)
+        {
+            double amount = 0;
+            IShoppingCartVisitor visit = new ShoppingCartVisitorImpl();
+            for (int i = 0; i < items.Length; i++)
+            {
+                amount += items[i].accept(visit);
+            }
+            return amount;
+        }
+    }
+}
