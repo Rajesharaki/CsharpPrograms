65a4f647150b54774a4d995a561d86a0c3d7ef0a
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 12:09:06 +0530

    [Rajesha T K]add. Implementation of IMediator interface code

---
+++ DesignPatterns/BehavioralDesignPattern/MediatorDesignPattern/IMediator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.MediatorDesignPattern
+{
+    public interface IMediator
+    {
+        public void Register(IUsers user);
+        public void SendMessage(String message,IUsers users);
+    }
+}
