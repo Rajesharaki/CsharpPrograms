02f0d4ef1cb2a60769f5ae10aaba37434d683409
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:11:02 +0530

    [Rajesha T K]add. Implementation of Fruit  program code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/Fruits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    public class Fruits : IItemElement
+    {
+        public string FruitName;
+        public int price;
+        public double Weight;
+        public Fruits(string FruitName,int price,double weight) 
+        {
+            this.FruitName = FruitName;
+            this.price = price;
+            this.Weight = weight;
+        }
+        public double accept(IShoppingCartVisitor Visitor)
+        {
+            return Visitor.Visit(this);
+        }
+    }
+}
