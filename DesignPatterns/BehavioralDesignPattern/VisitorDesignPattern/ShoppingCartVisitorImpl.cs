b934d310d43397382618db4fe281f7e80a93fe32
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-14 10:14:51 +0530

    [Rajesha T K]add. Implementation of ShoppingcartvisitorImpl program code

---
+++ DesignPatterns/BehavioralDesignPattern/VisitorDesignPattern/ShoppingCartVisitorImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.BehavioralDesignPattern.VisitorDesignPattern
+{
+    public class ShoppingCartVisitorImpl : IShoppingCartVisitor
+    {
+        public double Visit(Fruits fruit)
+        {
+            double sum = 0;
+            sum += fruit.Weight * fruit.price;
+            return sum;
+        }
+
+        public double Visit(Vegitables veg)
+        {
+            double sum = 0;
+            sum += veg.Weight * veg.price;
+            return sum;
+        }
+    }
+}
