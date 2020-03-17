a6362e1efee37183cecf4e0d6b75947427e089d1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 17:58:48 +0530

    [Rajesha T K]add. implementation of AutoMapper program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithSameName/AutoMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace DesignPatterns.AutoMapper
+{
+    public class AutoMapper
+    {
+        /*static void Main()
+        {
+            Person person = new Person("Madan", "Kumar", 6565465411);
+            //by using auto mapper we can tranfer or copy one class object content to the another class object coontent if content name is same
+            MapperConfiguration config = new MapperConfiguration(cfg => { cfg.CreateMap<Person, Employee>(); });
+            IMapper conf=config.CreateMapper();
+            Employee emp=conf.Map<Person,Employee>(person);
+            Console.WriteLine($"EmployeName: {emp.FirstName}\nLastName: {emp.LastName}\nPhoneNumber:{emp.PhoneNumber}");
+        }*/
+    }
+}
