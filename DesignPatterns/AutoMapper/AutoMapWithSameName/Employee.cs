2b1761d972b5113a26263a71df43424f6c6aae3d
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 17:57:42 +0530

    [Rajesha T K]add. implementation of Employee program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithSameName/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AutoMapper
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
