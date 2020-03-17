2369a38349c8bfbdfd0d1bf0d93adf8d1dc45981
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 17:53:04 +0530

    [Rajesha T K]add. implementation of Person program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithSameName/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AutoMapper
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+        public Person(String FirstName, String LastName, long PhoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+}
