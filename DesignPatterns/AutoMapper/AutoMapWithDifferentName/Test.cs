810b93191d490c8d53f94c0f3b67e11b6e74c016
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 18:04:41 +0530

    [Rajesha T K]add. implementation of Test program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithDifferentName/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace DesignPatterns.AutoMapper.AutoMapWithDifferentName
+{
+    public class Test
+    {
+        static void Main()
+        {
+            Aurthor aur = new Aurthor("sanju", "Giri", 152454554);
+            MapperConfiguration config = new MapperConfiguration(cfg => { cfg.CreateMap<Aurthor, AurthorDTO>()
+                .ForMember(dest=>dest.FN,opt=>opt.MapFrom(src=>src.FirstName))
+                .ForMember(dest=>dest.LN,opt=>opt.MapFrom(src=>src.LastName))
+                .ForMember(dest=>dest.PN,opt=>opt.MapFrom(src=>src.PhoneNumber)); });
+            IMapper mapper=config.CreateMapper();
+            AurthorDTO a=mapper.Map<Aurthor, AurthorDTO>(aur);
+            Console.WriteLine($"AurthorName: {a.FN}\nLastName: {a.LN}\nPhoneNumber:{a.PN}");
+        }
+    }
+}
