3390d20a9cff8f2781050d7908a389265f3784d2
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 18:00:35 +0530

    [Rajesha T K]add. Implemenatation of Aurthor program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithDifferentName/Aurthor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AutoMapper.AutoMapWithDifferentName
+{
+    public class Aurthor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+        public Aurthor(String FirstName, String LastName, long PhoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+}
