a283019870f9066c34c0a12341e9f454caecd628
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-17 18:04:03 +0530

    [Rajesha T K]add. implementation of AurthorDto program code

---
+++ DesignPatterns/AutoMapper/AutoMapWithDifferentName/AurthorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AutoMapper.AutoMapWithDifferentName
+{
+    public class AurthorDTO
+    {
+        public string FN { get; set; }
+        public string LN { get; set; }
+        public long PN { get; set; }
+    }
+}
