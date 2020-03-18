3621d1c1586986fa7bebdc7854213fbce892ced2
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-18 11:51:29 +0530

    [Rajesha T K]add. Implementation of EmployeeDataBase Program code

---
+++ DesignPatterns/DependencyInjection/EmployeeDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    class EmployeeDataBase
+    {
+        private static void Main(String[]args)
+        {
+            EmployeeDeatails EmpDetail1= new EmployeeDeatails(new Employee("Rajesha","Developer"));
+            EmpDetail1.GetDetails();
+            EmployeeDeatails EmpDetail2 = new EmployeeDeatails(new Employee("Sanju","Tester"));
+            EmployeeDeatails EmpDetail3 = new EmployeeDeatails(new Employee("Madan","Team Leader"));
+            EmpDetail2.GetDetails();
+            EmpDetail3.GetDetails();
+        }
+    }
+}
