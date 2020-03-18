fcf5f3642ee31d2fe53dcec1a72ab1fb643a8058
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-18 11:52:01 +0530

    [Rajesha T K]add. Implementation of EmployeeDetails Program code

---
+++ DesignPatterns/DependencyInjection/EmployeeDeatails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class EmployeeDeatails
+    {
+        Employee emp;
+        public EmployeeDeatails(Employee emp)
+        {
+            this.emp = emp;
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine($"EmployeName: {emp.EmployeeName}\nDepartMent: {emp.DepartMent}");
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+    }
+}
