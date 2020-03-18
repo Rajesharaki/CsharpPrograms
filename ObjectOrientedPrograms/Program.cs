bd2f43ead00ad0d3e628e2bcf02fe3a05dcc23c1
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-18 13:18:30 +0530

    [Rajesha T K]add. hidden files

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using ObjectOrientedPrograms.CommercialDataProcessing;
+using ObjectOrientedPrograms.InventoryManagementData;
+using ObjectOrientedPrograms.DeckOfCards;
+using System;
+
+namespace ObjectOrientedPrograms.StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the class number  ");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:new AddressBook();break;
+                case 2:new InventoryManagement();break;
+                case 3:new StockAccountManagement();break;
+                case 4:new CommericialDataManagement();break;
+                case 5:new DeckOfCard();break;
+                case 6:new DeckCardQueue();break;
+                default:Console.WriteLine("Invalid Class Number: ");break;
+            }
+        }
+    }
+}
