8537dc581c5576760e63db1a8695816c5bc736fc
RajeshaRaki <rajesharaki8613@gmail.com>
2020-03-18 11:50:50 +0530

    [Rajesha T K]add. Implementation of Employee Program code

---
+++ DesignPatterns/DependencyInjection/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string DepartMent { get; set; }
+        public Employee(String EmpName,String Dept)
+        {
+            this.EmployeeName = EmpName;
+            this.DepartMent = Dept;
+        }
+    }
+}
