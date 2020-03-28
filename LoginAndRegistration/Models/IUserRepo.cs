795e23db13b68746dc6bb647d22f43d3f143e6eb
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-03-28 14:17:13 +0530

    [Rajesha T K] add . Login and Regester folder

---
+++ LoginAndRegistration/Models/IUserRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplication.Models
+{
+    public interface IUserRepo
+    {
+        void Add(User user);
+        IEnumerable<User> GetUsers();
+        User GetUser(int id);
+        User Delete(int id);
+        User Update(User user);
+
+    }
+}
---
+++ LoginAndRegistration/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApplication.Models
+{
+    public class User
+    {
+        [Key, Display(Name="Identity:")]
+        public int ID { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Invalid Entry"), MaxLength(30),Display(Name ="Name: ")]
+        public string Name { get; set; }
+        [Required, EmailAddress,Display(Name ="Email Address: ")]
+        public string  Email { get; set; }
+    }
+}
