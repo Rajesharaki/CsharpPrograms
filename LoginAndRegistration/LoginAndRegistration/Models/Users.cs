9554b998791e766396b9c790125b71d29b85e930
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-01 00:46:54 +0530

    Add. all file

---
+++ LoginAndRegistration/LoginAndRegistration/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegistration.Models
+{
+    public class Users
+    {
+        [Key, Display(Name = "Identity:")]
+        public int ID { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Invalid Entry"), MaxLength(30), Display(Name = "Name: ")]
+        public string Name { get; set; }
+        [Required, EmailAddress, Display(Name = "Email Address: ")]
+        public string Email { get; set; }
+
+    }
+}
