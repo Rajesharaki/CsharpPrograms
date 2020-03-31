74f44b4f9c6890f7f8afafb5f1c14016d97b7e71
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-01 00:41:29 +0530

    [Rajesha T k]Refactor. modified AppDBContext program code

---
+++ LoginAndRegistration/LoginAndRegistration/Models/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace LoginAndRegistration.Models
+{
+    public class AppDBContext:IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> option):base(option)
+        {
+
+        }
+        public DbSet<Users> Users { get;set; }
+    }
+}
