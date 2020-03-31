cbe6cf7d3e69926f38314cabe4586ebb6f97985a
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-01 00:42:25 +0530

    [Rajesha T K]add. implementation of LoginviewModel code

---
+++ LoginAndRegistration/LoginAndRegistration/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegistration.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "This field is required")]
+        [EmailAddress(ErrorMessage = "Invalid format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name="Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
