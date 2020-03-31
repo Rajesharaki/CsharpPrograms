edf8ef62fca78e4b012fc7f9fe24dc3ae1f16a6f
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-03-31 19:16:15 +0530

    [Rajesha T K] add RegisterviewModel code

---
+++ LoginAndRegistration/LoginAndRegistration/Models/RegisterModelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class RegisterModelView
+    {
+       [Required(ErrorMessage ="This field is required")]
+       [EmailAddress(ErrorMessage ="Invalid format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password not match")]
+        [Display(Name ="Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
