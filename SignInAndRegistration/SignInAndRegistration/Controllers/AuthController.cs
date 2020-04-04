3dabeef6908ae88497dd4939789cd1e196545242
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-04 13:12:20 +0530

    [Rajesha T k]add. implementation of AuthController program code

---
+++ SignInAndRegistration/SignInAndRegistration/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SignInAndRegistration.ViewModel;
+
+namespace SignInAndRegistration.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> _usermanager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser>usermanager,IConfiguration configuration)
+        {
+            _usermanager = usermanager;
+            _configuration = configuration;
+        }
+        [Route("register")]
+        [HttpPost]
+        public async Task<ActionResult> Register([FromBody]RegisterViewModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await _usermanager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _usermanager.AddToRoleAsync(user, "Custmor");
+            }
+            return Ok(new { Username = user.UserName });
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult>Login([FromBody]LoginViewModel model)
+        {
+            var user = await _usermanager.FindByNameAsync(model.Username);
+            if (user != null && await _usermanager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName)
+                };
+                var signingKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["JWT:SigningKey"]));
+                var expiryInMinutes = Convert.ToInt32(_configuration["JWT:ExpiryInMinutes"]);
+
+                var Token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:Site"],
+                    audience: _configuration["JWT:Site"],
+                    expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256));
+                return Ok(
+                    new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(Token),
+                        expiration = Token.ValidTo
+                    });
+            }
+            return Unauthorized();
+        }
+    }
+}
