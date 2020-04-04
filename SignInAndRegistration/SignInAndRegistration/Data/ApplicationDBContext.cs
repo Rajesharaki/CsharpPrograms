ff51295b496512e5adeef1a1f7dd158f396b9ff0
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-04 13:11:12 +0530

    [Rajesha T K]add . implementation of Application Db context program code

---
+++ SignInAndRegistration/SignInAndRegistration/Data/ApplicationDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignInAndRegistration.Data
+{
+    public class ApplicationDBContext:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            #region "Seed Data"
+            builder.Entity<IdentityRole>().HasData(
+                new { Id="1",Name="Admin",NormalizedName="ADMIN"},
+                new { Id="2",Name="Custmor",NormalizedName="CUSTOMER"}
+             );
+            #endregion
+        }
+    }
+}
