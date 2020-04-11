d2841eda28d0adb716b1dd60c202d4e4d9dc14b1
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-11 18:04:29 +0530

    added FundooRepositary folder

---
+++ FundooAPI/FundooRepository/DBContext/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooRepository.DBContext
+{
+    public class AppDBContext:IdentityDbContext<IdentityUser>
+    {
+        public AppDBContext(DbContextOptions<AppDBContext>options):base(options)
+        {
+
+        }
+    }
+}
