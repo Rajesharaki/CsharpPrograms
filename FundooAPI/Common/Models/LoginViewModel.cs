e6ec4ac95037285a181c5b64d5626feef91aec6c
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-11 18:00:08 +0530

    added common folder

---
+++ FundooAPI/Common/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ FundooAPI/Common/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.Models
+{
+    public class ResetPasswordViewModel
+    {
+        public  string Email { get; set;}
+        [Required]
+        [DataType(DataType.Password)]
+        public  string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password Not Matched")]
+        public string ConfirmPassword { get; set; }
+        public string Token { get; set; }
+    }
+}
