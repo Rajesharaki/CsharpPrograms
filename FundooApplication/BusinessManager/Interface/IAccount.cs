a1b91a41d3f93b15c9d4c2326019ae328aee3c32
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-11 18:07:52 +0530

    added FundooAppplication folder

---
+++ FundooApplication/BusinessManager/Interface/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManager.Interface
+{
+    public interface IAccount
+    {
+        Task<IdentityResult> CreateUserAsync(RegisterViewModel model);
+        Task<SignInResult> SignInAsync(LoginViewModel model);
+        Task<string> ForgetPasswordAsync(ForgetPasswordViewModel model);
+        Task<IdentityResult> ResetPasswordAsync(ResetPasswordViewModel model);
+    }
+}
---
+++ FundooApplication/Common/Models/ForgetPasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.Models
+{
+    public class ForgetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
