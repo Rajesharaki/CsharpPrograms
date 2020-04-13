06bfc28aa873881912f4b9a93b27ec409675510f
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-13 19:01:49 +0530

    Added All folder

---
+++ FundooAPI/FundooAPI/Controllers/LabelsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationBusiness.Interfaces;
+using Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooAPI.Controllers
+{
+    /// <summary>
+    /// this is the Labels Controller class
+    /// </summary>
+    ////[Authorize]
+    [Route("api/[controller]")]
+    public class LabelsController : Controller
+    {
+        /// <summary>
+        /// LabelManager to manager the labels
+        /// </summary>
+        private readonly ILabelManager labelManager;
+
+        /// <summary>
+        /// Constructor injection of LabelsManager
+        /// </summary>
+        /// <param name="labelManager">DE of LabelsManager</param>
+        public LabelsController(ILabelManager labelManager)
+        {
+            this.labelManager = labelManager;
+        }
+
+        /// <summary>
+        /// Adds the new Label
+        /// </summary>
+        /// <param name="model">from body</param>
+        /// <returns>Result</returns>
+        [HttpPost, Route("add")]
+        public async Task<IActionResult> Add([FromBody]LabelsModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await this.labelManager.AddLabel(model);
+                if (result==1)
+                {
+                    return Ok(result);
+                }
+                return this.NotFound(result);
+             }
+            return this.BadRequest();
+        }
+
+        /// <summary>
+        /// updates the label
+        /// </summary>
+        /// <param name="model">from body</param>
+        /// <returns>Result</returns>
+        [HttpPut, Route("update")]
+        public async Task<IActionResult> Update([FromBody]LabelsModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await this.labelManager.UpdateLabel(model);
+                return Ok(result);
+            }
+            return this.NotFound();
+        }
+
+        /// <summary>
+        /// Gets the Label based on the id
+        /// </summary>
+        /// <param name="id">from body</param>
+        /// <returns>Result</returns>
+        [HttpGet, Route("get")]
+        public IActionResult Get([FromBody]int id)
+        {
+            var model = this.labelManager.GetLabel(id);
+            if (model != null)
+            {
+                return this.NotFound();
+            }
+            return this.Ok(model);
+        }
+
+        /// <summary>
+        /// Deletes the Label
+        /// </summary>
+        /// <param name="id">from body</param>
+        /// <returns>Result</returns>
+        [HttpDelete, Route("delete")]
+        public async Task<IActionResult> Delete([FromBody]int id)
+        {
+            var result = await this.labelManager.DeleteLabel(id);
+            if (result == 1)
+            {
+                return this.Ok(result);
+            }
+            return this.NotFound();
+        }
+
+        /// <summary>
+        /// gets all the labels
+        /// </summary>
+        /// <returns>Result</returns>
+        [HttpGet, Route("getall")]
+        public IActionResult GetAll()
+        {
+            var result = this.labelManager.GetAllLabels();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return this.NotFound();
+        }
+    }
+}
---
+++ FundooAPI/FundooRepository/Manager/LabelsRepositoryImpl.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationRepository.Context;
+using ApplicationRepository.Interfaces;
+using Common.Models;
+
+
+namespace ApplicationRepository.Services
+{
+    /// <summary>
+    /// this class implements the ILabelsRepository
+    /// </summary>
+    public class LabelsRepositoryImpl : ILabelsRepository
+    {
+        /// <summary>
+        /// private Field for DbContext object
+        /// </summary>
+        private readonly AppDbContext context;
+
+        /// <summary>
+        /// Constructor injection of DbContext
+        /// </summary>
+        /// <param name="context">Injects the DbContext object</param>
+        public LabelsRepositoryImpl(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Adds the New Label
+        /// </summary>
+        /// <param name="model">From body</param>
+        /// <returns>result</returns>
+        public async Task<int> AddLabel(LabelsModel model)
+        {
+            //// checking wether the same labelmodel exists in the table
+            var exists = this.context.Labels.FirstOrDefault(o => o.LBNumber == model.LBNumber);
+            
+            //// if not exists then adding the data to the table
+            if(exists==null)
+            {
+                await this.context.Labels.AddAsync(model);
+                return await this.context.SaveChangesAsync();
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Deltes the label based on Id
+        /// </summary>
+        /// <param name="id">from body</param>
+        /// <returns>Resul</returns>
+        public async Task<int> DeleteLabel(int id)
+        {
+            var result = this.context.Labels.FirstOrDefault(o => o.LBNumber == id);
+            if(result!=null)
+            {
+                this.context.Labels.Remove(result);
+                return await this.context.SaveChangesAsync();
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets all the Labels
+        /// </summary>
+        /// <returns>Labels</returns>
+        public IEnumerable<LabelsModel> GetAllLabels()
+        {
+            return this.context.Labels;
+        }
+
+        /// <summary>
+        /// Gets particular label based on id
+        /// </summary>
+        /// <param name="id">from body</param>
+        /// <returns>result</returns>
+        public LabelsModel GetLabel(int id)
+        {
+            var result = this.context.Labels.FirstOrDefault(o => o.LBNumber == id);
+            if(result!=null)
+            {
+                return result;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// update the Label if it exists
+        /// </summary>
+        /// <param name="id">from body</param>
+        /// <param name="label">new label from body</param>
+        /// <returns></returns>
+        public async Task<int> UpdateLabel(int id, string label)
+        {
+
+            var Label = this.context.Labels.FirstOrDefault(o => o.LBNumber == id);
+            if (Label != null && label != null)
+            {
+                Label.Label = label;
+                this.context.Labels.Update(Label);
+                return await this.context.SaveChangesAsync();
+            }
+            return 0;
+        }
+    }
+}
