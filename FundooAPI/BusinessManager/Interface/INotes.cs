c24af00fc485885d840e213dfbed57363b37d2e6
Rajesharaki <60682844+Rajesharaki@users.noreply.github.com>
2020-04-21 12:54:23 +0530

    [Rajesha]add. INotes interface

---
+++ FundooAPI/BusinessManager/Interface/INotes.cs
@@ -0,0 +1,24 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManager.Interface
+{
+    public interface INotes
+    {
+        Task<bool> AddNotesAsync(IFormFile file, NotesViewModel model);
+        Task<NotesViewModel> GetNotesByIdAsync(int id,string email);
+        IEnumerable<NotesViewModel> GetAllNotes(string email);
+        Task<bool> DeleteByIdAsync(int id,string email);
+        Task<bool> UpdateNotesAsync(IFormFile file, NotesViewModel model);
+        Task<bool>IsTrash(int id,string email);
+        Task<bool>IsArchive(int id, string email);
+        Task<bool> Pin(int id, string email);
+        Task<bool> Reminder(int id, string email);
+        Task<bool> DeleteAllNotes(string email);
+    }
+}
